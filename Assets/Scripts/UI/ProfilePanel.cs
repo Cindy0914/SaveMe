47f301015f62ee189efc0d6c163e22a86e339cb3
Cindy0914 <98309090+Cindy0914@users.noreply.github.com>
2025-01-21 19:39:10 +0900

    resource 적용

--- Assets/Scripts/UI/ProfilePanel.cs
+++ Assets/Scripts/UI/ProfilePanel.cs
@@ -7,18 +7,23 @@ public class ProfilePanel : MonoBehaviour
 {
     [SerializeField] private List<Profile> profiles = new();
     [SerializeField] private TimePanel timePanel;
-    
-    [TextArea(5, 10)] public List<string> descriptions = new();
-    
+
+    public List<string> names = new();
+    [TextArea(5, 10)]
+    public List<string> descriptions = new();
+
     public void Start()
     {
         InitTimePanel();
         InitProfile();
-        
+
         StopAllCoroutines();
         for (int i = 0; i < profiles.Count; i++)
         {
-            StartCoroutine(ShowTextOneByOne(profiles[i], descriptions[i]));
+            var profile = profiles[i];
+            var description = descriptions[i];
+            var name = names[i];
+            StartCoroutine(ShowTextOneByOne(profile, description, name));
         }
     }
 
@@ -31,32 +36,37 @@ public class ProfilePanel : MonoBehaviour
 
     private void InitProfile()
     {
-        if (profiles.Count != descriptions.Count)
+        const int teamCount = 4;
+        if (profiles.Count != teamCount)
         {
-            Debug.LogWarning("Profiles and descriptions count mismatch");
+            Debug.LogWarning("Profiles count mismatch");
             return;
         }
-        
-        for (int i = 0; i < profiles.Count; i++)
+        if (descriptions.Count != teamCount)
         {
-            var profile = profiles[i];
-            profile.tmpDesc.text = string.Empty;
-            profile.picture_01.SetActive(false);
-            profile.picture_02.SetActive(false);
+            Debug.LogWarning("descriptions count mismatch");
+            return;
         }
-        
+        if (names.Count != teamCount)
+        {
+            Debug.LogWarning("names count mismatch");
+            return;
+        }
+
         for (int i = 0; i < profiles.Count; i++)
         {
             var profile = profiles[i];
             profile.tmpDesc.text = string.Empty;
+            profile.tmpName.text = string.Empty;
             profile.picture_01.SetActive(false);
             profile.picture_02.SetActive(false);
         }
     }
-    
-    private IEnumerator ShowTextOneByOne(Profile profile, string description)
+
+    private IEnumerator ShowTextOneByOne(Profile profile, string description, string name)
     {
         yield return StartCoroutine(ShowPicture(profile));
+        yield return StartCoroutine(ShowTextName(profile, name));
         foreach (var letter in description)
         {
             profile.tmpDesc.text += letter;
@@ -67,10 +77,18 @@ public class ProfilePanel : MonoBehaviour
     private IEnumerator ShowPicture(Profile profile)
     {
         yield return new WaitForSeconds(0.5f);
-        
+
         profile.picture_01.SetActive(true);
-        yield return new WaitForSeconds(0.2f);
         profile.picture_02.SetActive(true);
         yield return new WaitForSeconds(0.2f);
+    }
+
+    private IEnumerator ShowTextName(Profile profile, string name)
+    {
+        foreach (var letter in name)
+        {
+            profile.tmpName.text += letter;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
