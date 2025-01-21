078576e5672ad2626e6ac5c0ce0b1c97de02cf10
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-01-21 16:13:05 +0900

    error code updata
    - 시간이 30초가 지난 후 멈추지 않은 점
    - DarkBox 게임 오브젝트 활성화 문제

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -16,7 +16,7 @@ public class Board : MonoBehaviour
             GameObject go = Instantiate(card, this.transform);
 
             float x = (i % 4) * 1.4f - 2.1f;
-            float y = (i / 4) * 1.4f - 3.0f;
+            float y = (i / 4) * 1.7f - 3.0f;
 
             go.transform.position = new Vector2(x, y);
             go.GetComponent<Card>().Setting(arr[i]);
