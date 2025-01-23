8ca189504ad6fe6b5ee5d9d9da5c6c2e1365f010
leesiyen <leesiyen02012@naver.com>
2025-01-23 15:10:23 +0900

    사운드파괴..

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance;
 
     public AudioSource audioSource;
     public AudioClip clip;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+
         audioSource.clip = this.clip;
         audioSource.Play();
     }
