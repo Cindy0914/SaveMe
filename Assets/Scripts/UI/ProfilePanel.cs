c051149cc2c0828f8835ee18bae05191fff7026f
Cindy0914 <98309090+Cindy0914@users.noreply.github.com>
2025-01-23 15:52:27 +0900

    사운드 적용

--- Assets/Scripts/UI/ProfilePanel.cs
+++ Assets/Scripts/UI/ProfilePanel.cs
@@ -1,26 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProfilePanel : MonoBehaviour
 {
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip buttonSound;
+    [SerializeField] private AudioClip textSound;
+
     [SerializeField] private List<Profile> profiles = new();
     [SerializeField] private TimePanel timePanel;
-    
-    private readonly WaitForSeconds wait_100ms = new(0.1f);
+    [SerializeField] private Button retryButton;
+
+    private readonly WaitForSeconds wait_50ms = new(0.05f);
     private float resultTime;
 
     public void Start()
     {
+        // Sound
+        audioSource = GetComponent<AudioSource>();
+        AddOnRetryButtonClick();
+
+        // Profile
         InitProfile();
         InitTimePanel();
 
+        // Coroutine
         StopAllCoroutines();
-        for (int i = 0; i < profiles.Count; i++)
-        {
-            var profile = profiles[i];
-            StartCoroutine(ShowTextOneByOne(profile));
-        }
+        StartCoroutine(TextCoroutine());
     }
 
     private void InitTimePanel()
@@ -29,6 +38,7 @@ public class ProfilePanel : MonoBehaviour
         {
             resultTime = PlayerPrefs.GetFloat("clearTime");
         }
+
         timePanel.SetTime(resultTime);
     }
 
@@ -49,31 +59,66 @@ public class ProfilePanel : MonoBehaviour
         }
     }
 
-    private IEnumerator ShowTextOneByOne(Profile profile)
+    private IEnumerator TextCoroutine()
     {
-        yield return new WaitForSeconds(0.3f);
-        yield return StartCoroutine(ShowTextName(profile));
+        PlayTextSound();
+        yield return ShowTextName();
+        yield return ShowTextOneByOne();
+        StopTextSound();
+    }
 
-        var description = profile.tmpDesc.text;
-        profile.tmpDesc.text = string.Empty;
-        profile.tmpDesc.gameObject.SetActive(true);
-        foreach (var letter in description)
+    private IEnumerator ShowTextOneByOne()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        for (int i = 0; i < profiles.Count; i++)
         {
-            profile.tmpDesc.text += letter;
-            yield return wait_100ms;
+            var profile = profiles[i];
+            var description = profile.tmpDesc.text;
+            
+            profile.tmpDesc.text = string.Empty;
+            profile.tmpDesc.gameObject.SetActive(true);
+            foreach (var letter in description)
+            {
+                profile.tmpDesc.text += letter;
+                yield return wait_50ms;
+            }
         }
     }
 
-    private IEnumerator ShowTextName(Profile profile)
+    private IEnumerator ShowTextName()
     {
-        var nameStr = profile.tmpName.text;
-        profile.tmpName.text = string.Empty;
-        profile.tmpName.gameObject.SetActive(true);
-        
-        foreach (var letter in nameStr)
+        for (int i = 0; i < profiles.Count; i++)
         {
-            profile.tmpName.text += letter;
-            yield return wait_100ms;
+            var profile = profiles[i];
+            var nameStr = profile.tmpName.text;
+            
+            profile.tmpName.text = string.Empty;
+            profile.tmpName.gameObject.SetActive(true);
+            foreach (var letter in nameStr)
+            {
+                profile.tmpName.text += letter;
+                yield return wait_50ms;
+            }
         }
+    }
+
+    private void PlayTextSound()
+    {
+        audioSource.clip = textSound;
+        audioSource.Play();
+        audioSource.loop = true;
+    }
+
+    private void StopTextSound()
+    {
+        audioSource.Stop();
+        audioSource.loop = false;
+        audioSource.clip = null;
+    }
+
+    private void AddOnRetryButtonClick()
+    {
+        retryButton.onClick.AddListener(() => { audioSource.PlayOneShot(buttonSound); });
     }
 }
